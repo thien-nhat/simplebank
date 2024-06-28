4e85378316bef2f12c3bd39045e6c92f592bfba1
Nhat Thien <76780335+ngonhatthien120@users.noreply.github.com>
2024-06-28 10:47:44 +0700

    Add createUser endpoint and update user_test.go

---
+++ api/user.go
@@ -0,0 +1,66 @@
+package api
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/lib/pq"
+	db "github.com/thien-nhat/simplebank/db/sqlc"
+	"github.com/thien-nhat/simplebank/util"
+)
+
+type CreateUserRequest struct {
+	Username string `json:"username" biding:"required,alphanum"`
+	Password string `json:"password" biding:"required,min=6"`
+	FullName string `json:"full_name" biding:"required"`
+	Email    string `json:"email" biding:"required,email"`
+}
+
+type CreateUserResponse struct {
+	Username          string    `json:"username"`
+	FullName          string    `json:"full_name"`
+	Email             string    `json:"email"`
+	PasswordChangedAt time.Time `json:"password_changed_at"`
+	CreatedAt         time.Time `json:"created_at"`
+}
+
+func (server *Server) createUser(ctx *gin.Context) {
+	var req CreateUserRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+	hashedPassword, err := util.HashPassword(req.Password)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+	arg := db.CreateUserParams{
+		Username:      req.Username,
+		HashedPassword: hashedPassword,
+		FullName:      req.FullName,
+		Email:         req.Email,
+
+	}
+	user, err := server.store.CreateUser(ctx, arg)
+	if err != nil {
+		if pqErr, ok := err.(*pq.Error); ok {
+			switch pqErr.Code.Name() {
+			case "unique_violation":
+				ctx.JSON(http.StatusForbidden, errorResponse(err))
+				return
+			}
+		}
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+	rsp := CreateUserResponse{
+		Username:          user.Username,
+		FullName:          user.FullName,
+		Email:             user.Email,
+		PasswordChangedAt: user.PasswordChangedAt,
+		CreatedAt:         user.CreatedAt,
+	}
+	ctx.JSON(http.StatusOK, rsp)
+}
